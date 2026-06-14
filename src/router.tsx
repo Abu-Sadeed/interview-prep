d5f3f7c49bc8a89df4363d357d1ec0513c4df28e
Abu Sadeed <abu.sadeed@technonext.com>
2026-06-14 17:27:30 +0600

    style: format imports and improve code readability in router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,20 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Layout } from './App';
-import { Overview } from './components/overview/Overview';
-import { BlockDetail } from './components/block/BlockDetail';
+import {createBrowserRouter} from 'react-router-dom';
+import {Layout} from './App';
+import {BlockDetail} from './components/block/BlockDetail';
+import {Overview} from './components/overview/Overview';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { index: true, element: <Overview /> },
-      { path: 'block/:id', element: <BlockDetail /> },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Layout />,
+			children: [
+				{index: true, element: <Overview />},
+				{path: 'block/:id', element: <BlockDetail />},
+			],
+		},
+	],
+	{
+		basename: '/interview-prep/',
+	},
+);
